fdbb52c4a109b696d8c0a4c89c3037d16a867c8e
Agustin Caamaño Flores <54273840+AgustinCaamanoFlores@users.noreply.github.com>
2025-07-05 23:06:19 -0300

    Fix route conflict

--- src/main/java/com/businessprosuite/api/controller/user/UserConsentController.java
+++ src/main/java/com/businessprosuite/api/controller/user/UserConsentController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/consents")
+@RequestMapping("/api/user-consents")
 @RequiredArgsConstructor
 public class UserConsentController {
 
